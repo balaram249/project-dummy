269997f80b189b14a549214a35f776760a11d4c4
balaram249 <69622981+balaram249@users.noreply.github.com>
2020-09-22 22:20:59 +0530

    Update App.java

--- HelloWorld/src/main/java/Visitcase/sender/App.java
+++ HelloWorld/src/main/java/Visitcase/sender/App.java
@@ -1,1 +1,14 @@
 
+package Visitcase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
